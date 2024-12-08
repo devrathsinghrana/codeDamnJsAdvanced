baf75c009130e9d525df348fe423ba0ce804bb90
devrathrana <devrath.rana@bold.com>
2024-12-09 00:29:49 +0530

    js

--- 5_asyncAwait.js
+++ 5_asyncAwait.js
@@ -8,3 +8,30 @@ const boot = async () => {
 };
 
 boot();
+
+const promise = async () => {
+  try {
+    Promise.reject("THIS IS my lovely error");
+  } catch (err) {
+    console.log(`My error is love: ${err}`);
+  }
+};
+
+promise();
+
+// async automatically indicates it is a promise
+const promise2 = async () => {
+  throw new Error("Hey");
+};
+
+promise2();
+console.log(
+  "This is error promise which will be printed as async is a promise so will go to micro queue."
+);
+
+const fun = () => {
+  throw new Error("Hey");
+};
+
+fun();
+console.log("This is fun not printed");

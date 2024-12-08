8c84ff229778d2ae7e63e8a12898f6d5d2033bb6
devrathrana <devrath.rana@bold.com>
2024-12-09 01:29:58 +0530

    js

--- 6_promiseAll.js
+++ 6_promiseAll.js
@@ -13,19 +13,26 @@ Run Tests
 */
 
 async function fn(...promises) {
-  const responses = await Promise.all([...promises]);
-  //   const requiredResponse = await Promise.all(responses.map((t) => t * 2));
-  const requiredResponse = await Promise.all(responses.map((t) => t.json()));
-  return requiredResponse;
+  try {
+    const responses = await Promise.all([...promises]);
+    //   const requiredResponse = await Promise.all(responses.map((t) => t * 2));
+    const requiredResponse = await Promise.all(responses.map((t) => t.json()));
+    return requiredResponse;
+  } catch (err) {
+    console.log("my new error to show: ", err);
+  }
 }
 
-const req1 = fetch("/data.json");
+const req1 = fetch("/dataa.json");//this will fail the whole promise
 const req2 = fetch("/data2.json");
 const req3 = fetch("/data3.json");
+const req4 = fetch("/dataa.json");//this will fail the whole promise
+const req5 = fetch("/data2.json");
+const req6 = fetch("/data3.json");
 
 (async () => {
   const fetchAll = fn(req1, req2, req3);
-  const fetchAllAwait = await fn(req1, req2, req3);
+  const fetchAllAwait = await fn(req4, req5, req6);
   console.log("fetchAll>>>", fetchAll);
   console.log("fetchAllAwait>>>", fetchAllAwait);
 })();
